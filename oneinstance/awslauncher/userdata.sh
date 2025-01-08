bca20836241e3caf77efb70d217a955b6106ea88
Enrique Molina <enrique.molina@urv.cat>
2025-01-08 16:34:32 +0100

    one fix

--- oneinstance/awslauncher/userdata.sh
+++ oneinstance/awslauncher/userdata.sh
@@ -3,15 +3,13 @@
 set -e
 set -x
 
-sudo apt-get update -y && sudo apt-get install -y python3-pip python3-venv
-sudo apt-get update -y && sudo apt-get install -y ffmpeg
-sudo apt-get install git -y
+apt-get update -y && apt-get install -y python3-pip python3-venv
+apt-get update -y && apt-get install -y ffmpeg
+apt-get install git -y
+apt-get install cargo -y
 
 # Clone the repository
 git clone "https://kikemolina3:<GH_TOKEN>@github.com/kikemolina3/autoscaling-korea.git"
-
-# Download the latest version of Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Add the cargo bin directory to the PATH
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
